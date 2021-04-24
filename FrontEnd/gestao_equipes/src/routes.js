1d3c64cb7bc8ded4535160b2d719e79ae9aadeb8
togarci <thyago.garcia10@gmail.com>
2021-04-24 17:06:45 -0300

    Se aplicado este commit ira inserir a pasta do front ao respositorio

---
+++ FrontEnd/gestao_equipes/src/routes.js
@@ -0,0 +1,29 @@
+import Principal from '@/pages/Principal.vue';
+
+const routes = [
+    {
+        name: 'Menu',
+        path: '/',
+        redirect: '/home',
+        component: Principal,
+        children: [
+            {
+                path: '/home',
+                name: 'home',
+                component: () => import('@/pages/Home.vue')
+            },
+            {
+                path: '/form',
+                name: 'formulario',
+                component: () => import('@/pages/Form.vue')
+            },
+            {
+                path: '/validacao',
+                name: 'validacao',
+                component: () => import('@/pages/Validation.vue')
+            }
+        ]
+    },
+]
+
+export default routes;
