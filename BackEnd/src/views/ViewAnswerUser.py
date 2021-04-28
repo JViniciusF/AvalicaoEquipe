a61edab4dad79cc24cc839593d8d5e3c9f1c0f31
togarci <thyago.garcia10@gmail.com>
2021-04-28 12:07:36 -0300

    Se aplicado este commit ira implemente o 'get' das respostas

--- BackEnd/src/views/ViewAnswerUser.py
+++ BackEnd/src/views/ViewAnswerUser.py
@@ -1,13 +1,18 @@
 from flask import Blueprint, request, jsonify
-from src.controller.Authenticate import jwt_required
-from src.controller.ValidateFieldsRequest import ValidateFieldsRequest
-from src.model.answer_user import Answer_User
 from datetime import datetime
 from src import db
+
+from src.controller.Authenticate import jwt_required
+from src.controller.ValidateFieldsRequest import ValidateFieldsRequest
+from src.controller.serializers.answer_serializer import Answer_Serializer
+
+from src.model.answer_user import Answer_User
+from src.model.user_groups import User_Groups
 
 import jwt, json
 
 validate = ValidateFieldsRequest()
+answer_serializer = Answer_Serializer()
 
 bp = Blueprint('answers', __name__, url_prefix='/answers')
 
@@ -30,6 +35,44 @@ def registerAnswer(current_user):
 
         return jsonify({ "status": "suas respostas foram salvas com sucesso" })
     
+    except Exception as e: 
+        error = { 'erro': f'{e}' }
+        return jsonify(error), 500
+
+@bp.route('/getAllAnswer', methods=['GET'])
+@jwt_required
+def getAllAnswer(current_user):
+    try:
+        role_current_user = User_Groups.query.filter_by(id=current_user.fk_id_user_group).first()
+
+        if role_current_user.group_name == 'Admin' or  role_current_user.group_name == 'Master':
+            data = answer_serializer.serializerAll()
+            return jsonify(data)
+        
+        else:
+            return jsonify({ "status": "Voce nao possui acecsso a essa transação" })
+
+    except Exception as e: 
+        error = { 'erro': f'{e}' }
+        return jsonify(error), 500
+
+@bp.route('/getAnswerByUser', methods=['GET'])
+@jwt_required
+def getAnswerByUser(current_user):
+    validateFields = validate.validateAll([ 'id_user' ], request)
+    if not validateFields['status']:
+        return jsonify({ "erro": validateFields['dataField'] }), 405
+    
+    try:
+        role_current_user = User_Groups.query.filter_by(id=current_user.fk_id_user_group).first()
+
+        if role_current_user.group_name == 'Admin' or role_current_user.group_name == 'Master':
+            data = answer_serializer.serializerByUser(request.json['id_user'])
+            return jsonify(data)
+
+        else:
+            return jsonify({ "status": "Voce nao possui acecsso a essa transação" })
+
     except Exception as e: 
         error = { 'erro': f'{e}' }
         return jsonify(error), 500
